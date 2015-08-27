2f627d3d24920ce4c71cc0712acab8a9a14a05fc
bianet13 <bia.macedo13@gmail.com>
2015-08-27 12:35:47 -0300

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.interactivestory.model;
+
+/**
+ * Created by Beatriz on 19/07/2015.
+ */
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
